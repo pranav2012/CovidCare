148ff1a05a8695b3d01bab39b4fddc3d5653d80e
Pranav Agarwal <p2012agarwal@gmail.com>
2021-04-19 12:25:43 +0530

    default template

---
+++ pages/_documnet.jsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
